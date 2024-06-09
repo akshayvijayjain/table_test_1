73980b8136d1c439253594a8ae170085c5970368
Akshay Jain <akshay.jain@zemosolabs.com>
2024-06-09 17:30:47 +0530

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,10 @@
+export default {
+	tbl_usersonSearchTextChanged (event) {
+		debugger;
+		console.log('search text changed. event details =',event);
+	},
+	imageClicked(event) {
+		debugger;
+		console.log('event: image clicked. event details = ',event);
+	}
+}
